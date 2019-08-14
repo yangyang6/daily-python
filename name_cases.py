de90bff0df915414e83eb033cef0b43ccd4036c9
yangyang6 <449542586@qq.com>
2019-08-14 16:28:58 +0800

    第二章读完总结

---
+++ name_cases.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    name = "Yang"
+    print("Hello " + name + ", would you like some to learn some Python today?")
+
+    print(name.upper())
+    print(name.lower())
+    #首字母大写
+    print(name.title())
---
+++ python2-8.py
@@ -0,0 +1,9 @@
+#import this算python的彩蛋吧，告诉你一些设计思想
+import this
+if __name__ == "__main__":
+    print(2*4)
+    #print(16 / 2)
+    #想获得整数的话，就用两个斜杠
+    print(16 // 2)
+    print(4+4)
+    print(8-0)
