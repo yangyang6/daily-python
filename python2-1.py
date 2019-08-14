35ab46316397923e0cf5958e203192e34b95130c
yangyang6 <449542586@qq.com>
2019-08-14 12:38:35 +0800

    python从入门到实践-第二章代码

---
+++ python2-1.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    message = "hello world"
+    print(message)
+    
+    message = "hello world2"
+    print(message)
+
+    name = "ada lovelace"
+    print(name.title())
+
+    #删除字符串尾部多余的空白
+    name = "dqw dqw "
+    print(name.rstrip() + "aaa")
